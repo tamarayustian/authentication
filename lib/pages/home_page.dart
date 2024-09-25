fc89ea103277a40fb9224eee659732ec15dbb4f0
Tamara Yustian <tamarayustian@gmail.com>
2024-09-25 23:04:50 +0800

    Make font and logo size to be responsive

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,21 +6,37 @@ class HomePage extends StatelessWidget {
 
   final user = FirebaseAuth.instance.currentUser!;
 
-// sign user out method
+  // Sign user out method
   void signUserOut() {
     FirebaseAuth.instance.signOut();
   }
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
-        appBar: AppBar(actions: [
-          IconButton(onPressed: signUserOut, icon: const Icon(Icons.logout))
-        ]),
-        body: Center(
-            child: Text(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: signUserOut,
+            icon: Icon(
+              Icons.logout,
+              size: screenSize.width < 600 ? 32 : 48, // Responsive icon size
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
           'LOGGED IN AS: ${user.email!}',
-          style: TextStyle(fontSize: 20),
-        )));
+          style: TextStyle(
+            fontSize: screenSize.width < 600 ? 16 : 24, // Responsive font size
+            fontWeight: FontWeight.bold, // Make text bold for emphasis
+          ),
+          textAlign: TextAlign.center, // Center align the text
+        ),
+      ),
+    );
   }
 }
