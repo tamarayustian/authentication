71e26e16c50d66a76937359e1cf6c74f1fc49698
Tamara Yustian <tamarayustian@gmail.com>
2024-09-25 23:13:22 +0800

    Add hover behaviour to buttons

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/material.dart';
 
-class MyButton extends StatelessWidget {
+class MyButton extends StatefulWidget {
   final Function()? onTap;
   final String text;
   const MyButton({super.key, required this.onTap, required this.text});
 
   @override
+  _MyButtonState createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  bool _isHovered = false; // Track the hover state
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: Container(
           padding: const EdgeInsets.all(25),
           margin: const EdgeInsets.symmetric(horizontal: 25.0),
           decoration: BoxDecoration(
-              color: Colors.black, borderRadius: BorderRadius.circular(8)),
+            color: _isHovered
+                ? Colors.grey[800]
+                : Colors.black, // Change color on hover
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+              color: _isHovered
+                  ? Colors.white
+                  : Colors.transparent, // Change border color on hover
+              width: 2, // Border width
+            ),
+          ),
           child: Center(
-              child: Text(text,
-                  style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16)))),
+            child: Text(
+              widget.text,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/components/square_tile.dart
+++ lib/components/square_tile.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 
-class SquareTile extends StatelessWidget {
+class SquareTile extends StatefulWidget {
   final String imagePath;
   final Function()? onTap;
   const SquareTile({super.key, required this.imagePath, required this.onTap});
 
   @override
+  _SquareTileState createState() => _SquareTileState();
+}
+
+class _SquareTileState extends State<SquareTile> {
+  bool _isHovered = false; // Track the hover state
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
             border: Border.all(color: Colors.white),
             borderRadius: BorderRadius.circular(16),
-            color: Colors.grey[200]),
-        child: Image.asset(
-          imagePath,
-          height: 72,
+            color: _isHovered
+                ? Colors.grey[300]
+                : Colors.grey[200], // Change color on hover
+          ),
+          child: Image.asset(
+            widget.imagePath,
+            height: 72,
+          ),
         ),
       ),
     );
